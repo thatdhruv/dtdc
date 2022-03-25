ecb141469c810a325c658b31710b3e21cdc58e60
thatdhruv <45945622+thatdhruv@users.noreply.github.com>
2022-03-25 09:02:32 +0530

    Update 2_usr.sh

--- 2_usr.sh
+++ 2_usr.sh
@@ -5,16 +5,6 @@ Phase 2 - User
 "
 source $HOME/dtdc/setup.conf
 
-sed -n '/'$INSTALL_TYPE'/q;p' ~/dtdc/packs.txt | while read line
-do
-	if [[ ${line} == '--END OF MINIMAL INSTALL--' ]]
-	then
-		continue
-	fi
-	echo "installing ${line}"
-	sudo pacman -S --noconfirm --needed ${line}
-done
-
 if [[ ! AUR_HELPER == none ]] ; then
 	cd ~
 	git clone "https://aur.archlinux.org/$AUR_HELPER.git"
@@ -23,6 +13,44 @@ if [[ ! AUR_HELPER == none ]] ; then
 fi
 
 export PATH=$PATH:~/.local/bin
+
+if [[ $INSTALL_TYPE == "full" ]] ; then
+	cd ~
+	mkdir .sources
+	cd .sources
+	
+	git clone https://github.com/thatdhruv/dwm
+	cd dwm
+	make
+	sudo make install
+	cd ..
+	
+	git clone https://github.com/thatdhruv/dmenu
+	cd dmenu
+	make
+	sudo make install
+	cd ..
+	
+	git clone https://github.com/thatdhruv/st
+	cd st
+	make
+	sudo make install
+	cd ..
+	
+	git clone https://github.com/thatdhruv/slstatus
+	cd slstatus
+	make
+	sudo make install
+	cd ..
+	
+	git clone https://github.com/thatdhruv/dotfiles
+	cd ~
+	cp -R dotfiles/* .
+	
+	git clone https://gitlab.com/dwt1/wallpapers
+	
+	rm -r .sources dotfiles
+fi
 
 echo -ne "
 Ready for phase 3
