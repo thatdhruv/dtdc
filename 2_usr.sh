b75d1200ec5af0e7462a181647d09e7a5e47487b
thatdhruv <45945622+thatdhruv@users.noreply.github.com>
2022-03-26 16:59:44 +0530

    Update 2_usr.sh

--- 2_usr.sh
+++ 2_usr.sh
@@ -17,55 +17,44 @@ echo -ne "
 █████████████████████████████████████████████████████████████
 [phase 2]\033[0m
 "
+
 source $HOME/dtdc/setup.conf
 
-if [[ ! AUR_HELPER == none ]] ; then
-	cd ~
-	git clone "https://aur.archlinux.org/$AUR_HELPER.git"
-	cd ~/$AUR_HELPER
-	makepkg -si --noconfirm
-fi
+echo -ne "
+\033[0;31m[setting up graphical environment]\033[0m
+"
+cd ~
+mkdir .sources
+cd .sources
+git clone https://github.com/thatdhruv/dwm
+cd dwm
+make
+sudo make install
+cd ..
+git clone https://github.com/thatdhruv/dmenu
+cd dmenu
+make
+sudo make install
+cd ..
+git clone https://github.com/thatdhruv/st
+cd st
+make
+sudo make install
+cd ..
+git clone https://github.com/thatdhruv/slstatus
+cd slstatus
+make
+sudo make install
+cd ~
+git clone https://github.com/thatdhruv/dotfiles
+cp -r dotfiles/. .
+rm -rf .sources dotfiles .git README.md
 
-export PATH=$PATH:~/.local/bin
+echo -ne "
+\033[0;31m[downloading wallpapers]\033[0m
+"
+git clone https://gitlab.com/dwt1/wallpapers
 
-if [[ $INSTALL_TYPE == "full" ]] ; then
-	cd ~
-	mkdir .sources
-	cd .sources
-	
-	git clone https://github.com/thatdhruv/dwm
-	cd dwm
-	make
-	sudo make install
-	cd ..
-	
-	git clone https://github.com/thatdhruv/dmenu
-	cd dmenu
-	make
-	sudo make install
-	cd ..
-	
-	git clone https://github.com/thatdhruv/st
-	cd st
-	make
-	sudo make install
-	cd ..
-	
-	git clone https://github.com/thatdhruv/slstatus
-	cd slstatus
-	make
-	sudo make install
-	cd ..
-	
-	cd ~
-	git clone https://github.com/thatdhruv/dotfiles
-	git clone https://gitlab.com/dwt1/wallpapers
-	cp -r dotfiles/. .
-	
-	rm -rf .sources dotfiles README.md .git
-fi
-
-clear
 echo -ne "
 \033[0;31m[ready for phase 3]\033[0m
 "
