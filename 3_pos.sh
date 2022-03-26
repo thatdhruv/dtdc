6f6bf4de5b86db7f91f0b5a7c2c8bd218525f361
thatdhruv <45945622+thatdhruv@users.noreply.github.com>
2022-03-26 11:01:07 +0530

    Update 3_pos.sh

--- 3_pos.sh
+++ 3_pos.sh
@@ -1,7 +1,21 @@
 #!/usr/bin/env bash
 
+clear
 echo -ne "
-Phase 3 - Post Install
+\033[0;31m█████████████████████████████████████████████████████████████
+█░░░░░░░░░░░░███░░░░░░░░░░░░░░█░░░░░░░░░░░░███░░░░░░░░░░░░░░█
+█░░▄▀▄▀▄▀▄▀░░░░█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀░░░░█░░▄▀▄▀▄▀▄▀▄▀░░█
+█░░▄▀░░░░▄▀▄▀░░█░░░░░░▄▀░░░░░░█░░▄▀░░░░▄▀▄▀░░█░░▄▀░░░░░░░░░░█
+█░░▄▀░░██░░▄▀░░█████░░▄▀░░█████░░▄▀░░██░░▄▀░░█░░▄▀░░█████████
+█░░▄▀░░██░░▄▀░░█████░░▄▀░░█████░░▄▀░░██░░▄▀░░█░░▄▀░░█████████
+█░░▄▀░░██░░▄▀░░█████░░▄▀░░█████░░▄▀░░██░░▄▀░░█░░▄▀░░█████████
+█░░▄▀░░██░░▄▀░░█████░░▄▀░░█████░░▄▀░░██░░▄▀░░█░░▄▀░░█████████
+█░░▄▀░░██░░▄▀░░█████░░▄▀░░█████░░▄▀░░██░░▄▀░░█░░▄▀░░█████████
+█░░▄▀░░░░▄▀▄▀░░█████░░▄▀░░█████░░▄▀░░░░▄▀▄▀░░█░░▄▀░░░░░░░░░░█
+█░░▄▀▄▀▄▀▄▀░░░░█████░░▄▀░░█████░░▄▀▄▀▄▀▄▀░░░░█░░▄▀▄▀▄▀▄▀▄▀░░█
+█░░░░░░░░░░░░███████░░░░░░█████░░░░░░░░░░░░███░░░░░░░░░░░░░░█
+█████████████████████████████████████████████████████████████
+[phase 3]\033[0m
 "
 source ${HOME}/dtdc/setup.conf
 
@@ -9,20 +23,23 @@ if [[ -d "/sys/firmware/efi" ]] ; then
 	grub-install --efi-directory=/boot ${DISK}
 fi
 
+clear
 echo -ne "
-Creating GRUB menu
+\033[0;31m[creating grub configuration]\033[0m
 "
 grub-mkconfig -o /boot/grub/grub.cfg
 echo -e "All set!"
 
+clear
 echo -ne "
-Enabling services
+\033[0;31m[enabling services]\033[0m
 "
 systemctl enable NetworkManager.service
 echo "NetworkManager enabled"
 
+clear
 echo -ne "
-Cleaning up
+\033[0;31m[cleaning up]\033[0m
 "
 sed -i 's/^%wheel ALL=(ALL) NOPASSWD: ALL/# %wheel ALL=(ALL) NOPASSWD: ALL/' /etc/sudoers
 sed -i 's/^%wheel ALL=(ALL:ALL) NOPASSWD: ALL/# %wheel ALL=(ALL:ALL) NOPASSWD: ALL/' /etc/sudoers
