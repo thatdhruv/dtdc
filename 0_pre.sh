3e8172f434db72ac10738c6844f9158078da583d
thatdhruv <45945622+thatdhruv@users.noreply.github.com>
2022-03-26 11:47:57 +0530

    Update 0_pre.sh

--- 0_pre.sh
+++ 0_pre.sh
@@ -89,10 +89,6 @@ cp -R ${DTDCDIR} /mnt/root/dtdc
 cp /etc/pacman.d/mirrorlist /mnt/etc/pacman.d/mirrorlist
 
 genfstab -L /mnt >> /mnt/etc/fstab
-echo "
-Generated /etc/fstab:
-"
-cat /mnt/etc/fstab
 
 clear
 echo -ne "
