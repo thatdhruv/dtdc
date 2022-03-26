8a175add6a00c7b2bbe21bcd574be4f1ce655eda
thatdhruv <45945622+thatdhruv@users.noreply.github.com>
2022-03-26 17:00:06 +0530

    Update 3_pos.sh

--- 3_pos.sh
+++ 3_pos.sh
@@ -17,39 +17,28 @@ echo -ne "
 █████████████████████████████████████████████████████████████
 [phase 3]\033[0m
 "
-source ${HOME}/dtdc/setup.conf
+
+source $HOME/dtdc/setup.conf
 
 if [[ -d "/sys/firmware/efi" ]] ; then
-	grub-install --efi-directory=/boot ${DISK}
+	grub-install --efi-directory=/boot ${DTDCDISK}
 fi
 
-clear
 echo -ne "
-\033[0;31m[creating grub configuration]\033[0m
+\033[0;31m[generating grub configuration]\033[0m
 "
 grub-mkconfig -o /boot/grub/grub.cfg
-echo -e "All set!"
 
-clear
 echo -ne "
 \033[0;31m[enabling services]\033[0m
 "
 systemctl enable NetworkManager.service
-echo "NetworkManager enabled"
 
-clear
 echo -ne "
 \033[0;31m[cleaning up]\033[0m
 "
-sed -i 's/^%wheel ALL=(ALL) NOPASSWD: ALL/# %wheel ALL=(ALL) NOPASSWD: ALL/' /etc/sudoers
 sed -i 's/^%wheel ALL=(ALL:ALL) NOPASSWD: ALL/# %wheel ALL=(ALL:ALL) NOPASSWD: ALL/' /etc/sudoers
-
-sed -i 's/^# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/' /etc/sudoers
 sed -i 's/^# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
-
-sed -i "s/^$USERNAME ALL=(ALL:ALL) NOPASSWD: ALL/#$USERNAME ALL=(ALL:ALL) NOPASSWD: ALL/" /etc/sudoers
-
-rm -r $HOME/dtdc
-rm -r /home/$USERNAME/dtdc
-
+sed -i "s/^$DTDCUSER ALL=(ALL:ALL) NOPASSWD: ALL/# $DTDCUSER ALL=(ALL:ALL) NOPASSWD: ALL/" /etc/sudoers
+rm -rf $HOME/dtdc /home/$DTDCUSER/dtdc
 cd $pwd
