652bb909207126452954c4655182a1ed09d9bb6e
thatdhruv <45945622+thatdhruv@users.noreply.github.com>
2022-03-26 16:59:22 +0530

    Update 1_set.sh

--- 1_set.sh
+++ 1_set.sh
@@ -17,152 +17,93 @@ echo -ne "
 █████████████████████████████████████████████████████████████
 [phase 1]\033[0m
 "
+
 source $HOME/dtdc/setup.conf
 
-clear
 echo -ne "
-\033[0;31m[network setup]\033[0m
+\033[0;31m[setting up network services]\033[0m
 "
-pacman -S --noconfirm --needed networkmanager
-systemctl enable NetworkManager
+pacman -S --noconfirm networkmanager
+systemctl enable --now NetworkManager
 
-clear
+nc=$(grep -c ^processor /proc/cpuinfo)
 echo -ne "
-\033[0;31m[setting up mirrors for optimal downloads]\033[0m
+\033[0;31m[setting up makeflags and compression settings for "$nc" nc]\033[0m
 "
-pacman -S --noconfirm --needed curl
-pacman -S --noconfirm --needed reflector grub git
-cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.old
-
-cores=$(grep -c ^processor /proc/cpuinfo)
-
-clear
-echo -ne "
-\033[0;31m[applying necessary modifications for "$cores" cores]\033[0m
-"
-TOTAL_MEM=$(cat /proc/meminfo | grep -i 'memtotal' | grep -o '[[:digit:]]*')
-if [[ $TOTAL_MEM -gt 8000000 ]] ; then
-	sed -i "s/#MAKEFLAGS=\"-j2\"/MAKEFLAGS=\"-j$cores\"/g" /etc/makepkg.conf
-	sed -i "s/COMPRESSXZ=(xz -c -z -)/COMPRESSXZ=(xz -c -T $cores -z -)/g" /etc/makepkg.conf
+DTDCTMEM=$(cat /proc/meminfo | grep -i 'memtotal' | grep -o '[[:digit:]]*')
+if [[ $DTDCTMEM -gt 8000000 ]] ; then
+	sed -i "s/#MAKEFLAGS=\"-j2\"/MAKEFLAGS=\"j$nc\"/g" /etc/makepkg.conf
+	sed -i "s/COMPRESSXZ=(xz -c -z -)/COMPRESSXZ=(xz -c -z -T $nc -)/g" /etc/makepkg.conf
 fi
 
-clear
 echo -ne "
-\033[0;31m[setting up language and locale]\033[0m
+\033[0;31m[setting up locale]\033[0m
 "
 sed -i 's/^#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
 locale-gen
-timedatectl --no-ask-password set-timezone ${TIMEZONE}
+timedatectl --no-ask-password set-timezone ${DTDCTIME}
 timedatectl --no-ask-password set-ntp 1
+localectl --no-ask-password set-keymap us
 localectl --no-ask-password set-locale LANG="en_US.UTF-8" LC_TIME="en_US.UTF-8"
-ln -s /usr/share/zoneinfo/${TIMEZONE} /etc/localtime
-localectl --no-ask-password set-keymap ${KEYMAP}
-
-sed -i 's/^# %wheel ALL=(ALL) NOPASSWD: ALL/%wheel ALL=(ALL) NOPASSWD: ALL/' /etc/sudoers
-sed -i 's/^# %wheel ALL=(ALL:ALL) NOPASSWD: ALL/%wheel ALL=(ALL:ALL) NOPASSWD: ALL' /etc/sudoers
-
+ln -s /usr/share/zoneinfo/${DTDCTIME} /etc/localtime
+sed -i 's/^# %wheel ALL=(ALL:ALL) NOPASSWD: ALL/%wheel ALL=(ALL:ALL) NOPASSWD: ALL/' /etc/sudoers
 sed -i 's/^#ParallelDownloads/ParallelDownloads/' /etc/pacman.conf
 
-sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf
-pacman -Sy --noconfirm --needed
-
-clear
 echo -ne "
-\033[0;31m[installing utilities]\033[0m
+\033[0;31m[installing packages]\033[0m
 "
-if [[ ! $INSTALL_TYPE == minimal ]] ; then
-	sed -n '/'$INSTALL_TYPE'/q;p' $HOME/dtdc/packs.txt | while read line
+while read line
 do
-	if [[ ${line} == '--end of minimal install--' ]] ; then
-		continue
-	fi
-	echo "installing ${line}"
+	echo "[installing ${line}]"
 	sudo pacman -S --noconfirm --needed ${line}
 done
-fi
 
-clear
 echo -ne "
 \033[0;31m[installing microcode]\033[0m
 "
 proc_type=$(lscpu)
 if grep -E "GenuineIntel" <<< ${proc_type} ; then
-	echo "Installing Intel microcode"
+	echo "[installing intel microcode]"
 	pacman -S --noconfirm --needed intel-ucode
 	proc_ucode=intel-ucode.img
 elif grep -E "AuthenticAMD" <<< ${proc_type} ; then
-	echo "Installing AMD microcode"
+	echo "[installing amd microcode]"
 	pacman -S --noconfirm --needed amd-ucode
 	proc_ucode=amd-ucode.img
 fi
 
-gpu_type=$(lspci)
-if grep -E "NVIDIA|GeForce" <<< ${gpu_type} ; then
+echo -ne "
+\033[0;31m[installing graphics drivers]\033[0m
+"
+gpu=$(lspci)
+if grep -E "NVIDIA|GeForce" <<< ${gpu} ; then
 	pacman -S --noconfirm --needed nvidia
 	nvidia-xconfig
 elif lspci | grep 'VGA' | grep -E "Radeon|AMD" ; then
 	pacman -S --noconfirm --needed xf86-video-amdgpu
-elif grep -E "Integrated Graphics Controller" <<< ${gpu_type} ; then
-	pacman -S --noconfirm --needed libva-intel-driver libvdpau-va-gl lib32-vulkan-intel vulkan-intel libva-utils lib32-mesa
-elif grep -E "Intel Corporation UHD" <<< ${gpu_type} ; then
-	pacman -S --noconfirm --needed libva-intel-driver libvdpau-va-gl lib32-vulkan-intel vulkan-intel libva-utils lib32-mesa
+elif grep -E "Intel Graphics Controller" <<< ${gpu} ; then
+	pacman -S --noconfirm --needed xf86-video-intel
+elif grep -E "Intel Corporation UHD" <<< ${gpu} ; then
+	pacman -S --noconfirm --needed xf86-video-intel
 fi
 
-if ! source $HOME/dtdc/setup.conf ; then
-	while true
-	do
-		read -p "Please enter username: " username
-		if [[ "${username,,}" =~ ^[a-z_]([a-z0-9_-]{0,31}|[a-z0-9_-]{0,30}\$)$ ]]
-		then
-			break
-		fi
-		echo "Invalid username!"
-	done
-
-	echo "username=${username,,}" >> ${HOME}/dtdc/setup.conf
-	read -p "Please enter password: " password
-	echo "password=${password,,}" >> ${HOME}/dtdc/setup.conf
-
-	while true
-	do
-		read -p "Please enter machine name: " name_of_machine
-		if [[ "${name_of_machine,,}" =~ ^[a-z][a-z0-9_.-]{0,62}[a-z0-9]$ ]]
-		then
-			break
-		fi
-		read -p "Invalid hostname format. Do you still want to save it? (y/n)" force
-		if [[ "${force,,}" = "y" ]]
-		then
-			break
-		fi
-	done
-
-	echo "NAME_OF_MACHINE=${name_of_machine,,}" >> ${HOME}/dtdc/setup.conf
-fi
-
-clear
 echo -ne "
-\033[0;31m[adding user]\033[0m
+\033[0;31m[setting up user]\033[0m
 "
 if [ $(whoami) = "root" ] ; then
-	groupadd libvirt
-	useradd -m -G wheel,libvirt -s /bin/bash $USERNAME
-	echo "User $USERNAME created, home directory created, added to groups wheel and libvirt, default shell set to /bin/bash"
-
-	echo "$USERNAME:$PASSWORD" | chpasswd
-	echo "Password for $USERNAME has been set successfully"
-	echo "$USERNAME ALL=(ALL:ALL) NOPASSWD: ALL" >> /etc/sudoers
-
-	cp -R $HOME/dtdc /home/$USERNAME/
-	chown -R $USERNAME: /home/$USERNAME/dtdc
-	echo "dtdc copied to home directory"
-
-	echo $NAME_OF_MACHINE > /etc/hostname
+	useradd -m -G wheel -s /bin/bash $DTDCUSER
+	echo "[successfully added user $DTDCUSER]"
+	echo "$DTDCUSER|$DTDCPASS" | chpasswd
+	echo "[successfully set password for $DTDCUSER]"
+	echo "$DTDCUSER ALL=(ALL:ALL) NOPASSWD: ALL" >> /etc/sudoers
+	cp -R $HOME/dtdc /home/$DTDCUSER/
+	chown -R $DTDCUSER: /home/$DTDCUSER/dtdc
+	echo "[dtdc copied to home directory]"
+	echo $DTDCHOST > /etc/hostname
 else
-	echo "You already seem to be a user. Proceeding with PHASE 2..."
+	echo "You already seem to be a user. Proceeding with phase 2..."
 fi
 
-clear
 echo -ne "
 \033[0;31m[ready for phase 2]\033[0m
 "
