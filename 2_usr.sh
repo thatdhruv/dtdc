ad4d6338fa795823c7e6498e29e92301c6f33748
thatdhruv <45945622+thatdhruv@users.noreply.github.com>
2024-01-15 15:06:09 +0530

    Update 2_usr.sh

--- 2_usr.sh
+++ 2_usr.sh
@@ -45,24 +45,10 @@ git clone https://github.com/thatdhruv/slstatus
 cd slstatus
 make
 sudo make install
-cd ..
-git clone https://aur.archlinux.org/otf-san-francisco-mono.git
-cd otf-san-francisco-mono
-makepkg -si --noconfirm --needed
-cd ..
-git clone https://github.com/Karmenzind/monaco-nerd-fonts
-cd monaco-nerd-fonts && cd fonts
-sudo cp 'Monaco Nerd Font Complete Mono.ttf' /usr/share/fonts/apple/'Monaco Nerd Font Complete Mono.ttf'
-sudo fc-cache -fv
 cd ~
 git clone https://github.com/thatdhruv/dotfiles
 cp -r dotfiles/. .
 rm -rf .sources dotfiles .git README.md
-
-echo -ne "
-\033[0;31m[downloading wallpapers]\033[0m
-"
-git clone https://gitlab.com/dwt1/wallpapers .wallpapers
 
 echo -ne "
 \033[0;31m[ready for phase 3]\033[0m
